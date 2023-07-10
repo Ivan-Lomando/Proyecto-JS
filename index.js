b20ba724b94e34e69d6c48f720a487aebead3445
Ivan-Lomando <IVAN.LOMANDO@correo.ucu.edu.uy>
2023-07-10 08:51:23 -0300

    entrega 2

--- index.js
+++ index.js
@@ -1,41 +1,50 @@
-
-var carrito = [];
+let listaProductos = [];
+let carrito = [];
 let total = 0;
-/*
-class Producto{
-    constructor(nombre, precio){
-        this.nombre = nombre
-        this.precio = parseFloat(precio)
-        
+
+class Producto {
+    constructor(nombre, precio) {
+        this.nombre = nombre;
+        this.precio = parseFloat(precio);
     }
-    sumarIva(){
-        this.precio = this.precio * 1.21
+
+    sumarIva() {
+        this.precio = this.precio * 1.21;
     }
 }
-function agregaralCarrito(Producto){
-    carrito.push(Producto.precio);
-    return carrito
+
+function agregarProducto(nombre, precio) {
+    let producto = new Producto(nombre, precio);
+    listaProductos.push(producto);
+    console.log('Producto agregado al sistema:', producto);
 }
 
-function calcularTotal(carrito){
-    for (var i = 0; i < carrito.length; i++)
-    total = total + carrito[i];
-    return total
+function agregarAlCarrito(producto) {
+    carrito.push(producto);
 }
-const producto1 = new Producto("Product 1", 10.0);
-const producto2 = new Producto("Product 1", 10.0);
-agregaralCarrito(producto1);
-agregaralCarrito(producto2);
-alert(total);
 
-*/
+function calcularTotal(carrito) {
+    total = 0;
+    for (let i = 0; i < carrito.length; i++) {
+        total += carrito[i].precio;
+    }
+    return total;
+}
 
+let agregarMasProductos = true;
 
-let precio1 = prompt("ingrese el precio del producto:")
+while (agregarMasProductos) {
+    let nombreProducto = prompt('Ingrese el nombre del producto:');
+    let precioProducto = parseFloat(prompt('Ingrese el precio del producto:'));
+    
+    agregarProducto(nombreProducto, precioProducto);
+    agregarAlCarrito(listaProductos[listaProductos.length - 1]);
+    
+    let respuesta = prompt('¿Desea agregar otro producto al carrito? (Sí/No)');
+    if (respuesta.toLowerCase() !== 'sí' && respuesta.toLowerCase() !== 'si') {
+        agregarMasProductos = false;
+    }
+}
 
-
-total = total + parseFloat(precio1);
-
-alert("su carrito tiene un valor de " + total);
-
-
+total = calcularTotal(carrito);
+alert('Total del carrito: ' + total);
